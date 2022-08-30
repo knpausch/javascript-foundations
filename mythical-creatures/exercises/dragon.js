a6d0b610adb54c98ec89b0bcb69ad72a12f6540b
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-08-29 18:26:58 -0700

    Created class Dragon and 2 methods, passed all tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,27 @@
+var foodCounter = 0;
 
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+  }
+
+  greet()
+  {
+    var greeting = `Hi, ${this.rider}!`
+    return greeting;
+  }
+
+  eat()
+  {
+    foodCounter++;
+
+    if (foodCounter >= 3)
+    {
+      this.hungry = false;
+    }
+  }
+
+}
+module.exports = Dragon;
