c1ea78ce41486e54d8af6a7bde4c938a73fd4925
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-08-31 19:44:51 -0700

    create functions performIncantation, performTrick, and performShowStopper which passes all remaining tests

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -1,10 +1,34 @@
 class Magician {
-  constructor({name: stageName, assistant: hasAssistant}){
+  constructor({name: stageName, assistant: hasAssistant, clothing: clothes}){
     this.name = `The Great ${stageName}`;
     this.assistant = hasAssistant;
+    this.favoriteAccessory = clothes || "top hat";
+    this.confidencePercentage = 10;
   }
 
-}
+  performIncantation(trickName){
+    return `${trickName.toUpperCase()}!`;
+  }
 
+  performTrick(){
+    this.confidencePercentage+=10;
+    if(this.favoriteAccessory === "top hat"){
+      return "PULL RABBIT FROM TOP HAT";
+
+    }
+    else{
+      return "PULL DOVE FROM SLEEVE";
+    }
+  }
+
+  performShowStopper(){
+    if(this.confidencePercentage>=100 && this.assistant === true){
+      return "WOW! The magician totally just sawed that person in half!";
+    }
+    else{
+      return "Oh no, this trick is not ready!";
+    }
+  }
+}
 
 module.exports = Magician;
