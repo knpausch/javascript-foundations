d7bcbcc7d671aae6a7562e21c917a8ef247df98b
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-08-31 20:47:44 -0700

    creates class Medusa, Person and Statue with properties and accompaning functions that passes all tests

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,27 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa{
+  constructor(name){
+    this.name = name;
+    this.statues = [];
+  }
+
+  gazeAtVictim(victim){
+    var newStatue = new Statue(victim.name);
+
+    if(this.statues.length<3){
+      this.statues.push(newStatue);
+    }
+    else{
+      var stoneName = this.statues[0].name;
+      var freedPerson = new Person (stoneName, "relieved");
+      this.statues.shift();
+      this.statues.push(newStatue);
+      return freedPerson;
+    }
+  }
+
+}
+
+module.exports = Medusa;
