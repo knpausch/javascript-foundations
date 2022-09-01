534eb9191f5e08cb38f4a0e175508284015c5898
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-08-31 19:13:15 -0700

    Created Magician class and set name and assistant properties. Need to continue onto next test

---
+++ mythical-creatures/exercises/magician.js
@@ -0,0 +1,10 @@
+class Magician {
+  constructor({name: stageName, assistant: hasAssistant}){
+    this.name = `The Great ${stageName}`;
+    this.assistant = hasAssistant;
+  }
+
+}
+
+
+module.exports = Magician;
