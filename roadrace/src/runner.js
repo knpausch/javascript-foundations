fa915b3b9620f975507d10edb005b83bd68f1ff9
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-09-14 12:58:17 -0700

    completes runner.js file

--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,4 +1,27 @@
 class Runner {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.milesRun = 0;
+    this.fitness = 25;
+    this.completedRaces = [];
+  }
+
+  runSomeMiles(miles){
+    this.milesRun += miles;
+    this.fitness += miles;
+  }
+
+  stretch(){
+    this.fitness += 0.5;
+  }
+
+  runRace(name, length)
+  {
+    this.completedRaces.push(name);
+    this.milesRun += length;
+    this.fitness += length;
+  }
 
 }
 
