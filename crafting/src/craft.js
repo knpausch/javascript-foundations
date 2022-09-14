baadf9641571cf9edcc8c6682d51ee41e19590bc
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-09-14 12:29:51 -0700

    completes crafting and updates ReadMe files

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,26 @@
 class Craft {
+  constructor(craftItem){
+    this.name = craftItem.type;
+    this.materials = craftItem.materials;
+    this.completed = false;
+  }
 
+  completeCraft(){
+    this.completed = true;
+    return "All done! It looks great!";
+  }
+
+  calculateProjectTotal(){
+    var total;
+    var material1Price = this.materials[0].price;
+    var material1Amount = this.materials[0].amount;
+
+    var material2Price = this.materials[1].price;
+    var material2Amount = this.materials[1].amount;
+
+    total = (material1Price * material1Amount) + (material2Price * material2Amount);
+    return total;
+  }
 }
 
 module.exports = Craft;
