10958b735f5a49792b64c739046a7e4ba393e7fe
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-09-20 17:22:14 -0700

    finished golf course and golfer

--- golf-course/golf-course.js
+++ golf-course/golf-course.js
@@ -1,5 +1,24 @@
 class GolfCourse {
+  constructor(name, difficulty, openings, features){
+    this.name = name;
+    this.difficulty = difficulty;
+    this.openings = openings;
+    this.features = features;
+    this.currentlyPlaying = [];
+  }
 
+  checkInGroup(listOfPlayers){
+    if(listOfPlayers.length<this.openings){
+      this.openings -= listOfPlayers.length;
+      for(var i = 0; i<listOfPlayers.length;i++){
+          this.currentlyPlaying.unshift(listOfPlayers[i].name);
+      }
+      return "You\'re checked in. Have fun!";
+    }
+    else{
+      return "Sorry, we are currently booked! Please come back later.";
+    }
+  }
 
 }
 
